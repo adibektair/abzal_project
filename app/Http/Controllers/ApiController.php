e97351e7ac03ee1c6d66ddb7909ec118b803307d
sauleshka <zh.saule.01@gmail.com>
2019-11-13 20:50:00 +0600

    API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use App\City;
 use App\Country;
+use App\Excursion;
+use App\ExtraInfo;
+use App\Gender;
+use App\Language;
+use App\Level;
+use App\Messenger;
+use App\Value;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\Array_;
 
@@ -22,12 +29,47 @@ class ApiController extends Controller
 		return $this->makeResponse(200, true, ["cities" => $cities]);
     }
 
+    public function getGenders(){
+        $genders = Gender::all();
+        return $this->makeResponse(200, true, ['genders' => $genders]);
+    }
 
+    public function getMessengers(){
+        $messengers = Messenger::all();
+        return $this->makeResponse(200, true, ['messengers' => $messengers]);
+    }
 
+    public function getValues(){
+        $values = Value::all();
+        return $this->makeResponse(200, true, ['values' => $values]);
+    }
 
+    public function getLanguages(){
+        $languages = Language::all();
+        return $this->makeResponse(200, true, ['languages' => $languages]);
+    }
+
+    public function getLevel(Request $request){
+        $levels = Level::all();
+        return $this->makeResponse(200, true, ['levels' => $levels]);
+    }
+
+    public function getExtraInfos(){
+        $extra_infos = ExtraInfo::all();
+        return $this->makeResponse(200, true, ['extra_infos' => $extra_infos]);
+    }
+
+    public function getExcursion(){
+        $excursion = Excursion::all();
+        return $this->makeResponse(200, true, ['excoursions' => $excursion]);
+    }
 
     public function makeResponse($code, $success, Array $other){
         $json = array_merge($other, ['success' => $success]);
         return response()->json($json)->setStatusCode($code);
+    }
+
+    public function singUp(Request $request){
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/get-countries', ['uses' => 'ApiController@getCountries']);
 Route::post('/get-cities', ['uses' => 'ApiController@getCityByCountryId']);
+Route::get('/get-genders', ['uses' => 'ApiController@getGenders']);
+Route::get('/get-messengers', ['uses' => 'ApiController@getMessengers']);
+Route::get('/get-values', ['uses' => 'ApiController@getValues']);
+Route::get('/get-languages', ['uses' => 'ApiController@getLanguages']);
+Route::get('/get-level', ['uses' => 'ApiController@getLevel']);
+Route::get('/get-extra_infos', ['uses' => 'ApiController@getExtraInfos']);
+Route::get('/get-excursion', ['uses' => 'ApiController@getExcursion']);
+
